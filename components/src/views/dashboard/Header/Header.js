05cf04a09f0649c45a3a29bd48634ab95be61778
Le Ba Thien <thienlb31dc18@st.buh.edu.vn>
2022-07-02 13:12:07 +0700

    fix

--- components/src/views/dashboard/Header/Header.js
+++ components/src/views/dashboard/Header/Header.js
@@ -4,9 +4,18 @@ const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <div className="flex">
+        <div className="flex space-between center-y">
           <div className="logo">logo</div>
-          <div className="user">User</div>
+          <div className="user">
+            <a className="flex center-y" href="#">
+              <img
+                className="avatar"
+                src="https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+                alt="avatar"
+              />
+              <span>Username</span>
+            </a>
+          </div>
         </div>
       </div>
     </header>
